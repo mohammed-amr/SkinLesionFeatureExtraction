c64f2e7042464b2696273e2065b6d15af82d5298
Mohamed Sayed <mohammedamr94@gmail.com>
2019-11-29 15:56:32 +0000

    Clean up.

--- SampleCodesnippets.m
+++
@@ -1,7 +0,0 @@
-% %region growing
-% figure;
-% testregion = regiongrowing(im2double(imgray), sizeX/2, sizeY/2,0.2);
-% imshow(imfill(testregion, 'holes'));
-
- SE = strel('disk', round(((1/50)*sqrt(sizeX*sizeY))));
-imshow(imclose(FilledIn, SE));
